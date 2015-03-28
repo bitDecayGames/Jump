6fdcb00a2e8739482afae0193d674a733a5f7ed8
Logan Moore <monday.hopscotch@gmail.com>
2015-03-27 23:49:58 -0600

    First assets. Dat yellow dude.

--- Kenney/src/bitDecay/jump/game/GameScreen.java
+++ Kenney/src/bitDecay/jump/game/GameScreen.java
@@ -4,22 +4,32 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 import com.bitdecay.engine.utilities.ImageUtilities;
 
 
 public class GameScreen implements Screen {
 
-//	Animation running;
+	Animation running;
+	SpriteBatch batch = new SpriteBatch();
+	float elapsed = 0;
 	
 	@Override
 	public void show() {
-//		running = new Animation(.1f, ImageUtilities.getImagesByPrefix("player", "run"));
+		running = new Animation(.1f, ImageUtilities.getImagesByPrefix("yellow", "walk"));
+		running.setPlayMode(PlayMode.LOOP);
 	}
 
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0.0f, 0.8f, 0.0f, 1.0f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		batch.begin();
+		elapsed += delta;
+		batch.draw(running.getKeyFrame(elapsed), 100, 100);
+		batch.end();
 	}
 
 	@Override
--- Kenney/src/bitDecay/jump/game/Kenney.java
+++ Kenney/src/bitDecay/jump/game/Kenney.java
@@ -1,11 +1,16 @@
 package bitDecay.jump.game;
 
 import com.badlogic.gdx.Game;
+import com.bitdecay.engine.GameFactory;
+import com.bitdecay.engine.utilities.DevConstants;
+import com.bitdecay.engine.utilities.DevConstants.Constants;
+import com.bitdecay.engine.utilities.ImageUtilities;
 
 public class Kenney extends Game {
 
 	@Override
 	public void create() {
+		ImageUtilities.initialize();
 		setScreen(new GameScreen());
 	}
 
