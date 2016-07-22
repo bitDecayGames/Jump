6866778881349ff7c9d6ef8e866fcbfc4bbba345
Logan Moore <monday.hopscotch@gmail.com>
2016-07-21 23:13:39 -0600

    Move controllers to a pull-based model

--- jump-gdx/src/main/java/com/bitdecay/jump/gdx/input/ControllerButtonState.java
+++ jump-gdx/src/main/java/com/bitdecay/jump/gdx/input/ControllerButtonState.java
@@ -1,28 +1,60 @@
 package com.bitdecay.jump.gdx.input;
 
 import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.ControllerAdapter;
 
 /**
  * Created by MondayHopscotch on 7/20/2016.
  */
-public class ControllerButtonState implements InputStateReporter {
+public class ControllerButtonState extends ControllerAdapter implements InputStateReporter {
 
     private Controller controller;
     private int button;
+
+    private boolean buttonDown = false;
+    private boolean fresh = false;
 
 
     public ControllerButtonState(Controller controller, int button) {
         this.controller = controller;
         this.button = button;
+        controller.addListener(this);
+    }
+
+    @Override
+    public boolean buttonDown(Controller controller, int buttonCode) {
+        if (this.controller == controller && this.button == buttonCode) {
+            buttonDown = true;
+            fresh = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean buttonUp(Controller controller, int buttonIndex) {
+        if (this.controller == controller && this.button == buttonIndex) {
+            buttonDown = false;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public boolean isJustPressed() {
-        return controller.getButton(button);
+        if (buttonDown && fresh) {
+            fresh = false;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public boolean isPressed() {
-        return controller.getButton(button);
+        fresh = false;
+        return buttonDown;
     }
 }
